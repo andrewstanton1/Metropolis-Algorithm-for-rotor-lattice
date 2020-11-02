40471ba03b79bd5df35d34dc3deac47ea7d8ca85
Andrew Stanton <andrew.stanton@ucla.edu>
2020-11-01 20:26:29 -0800

    Update parameters.py
    Added comments

--- rotor-model/parameters.py
+++ rotor-model/parameters.py
@@ -1,7 +1,7 @@
-N = 16
-num_data_points = 10
-l_range = 0.6
-r_range = 2.6
-equalibration_steps = 10000
-steps_to_average = 1000
-rotational_potential = 0  #18.80436397
+N = 16                                            # NxN size of lattice
+num_data_points = 10                              # Number of data points
+l_range = 0.6                                     # starting data value
+r_range = 2.6                                     # ending data value
+equalibration_steps = 10000                       # number of monte carlo steps for equlibration 
+steps_to_average = 1000                           # number of monte carlo steps to average over
+rotational_potential = 0                          #18.80436397 # coefficient of rotation potential
