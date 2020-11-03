a846b66284d324c1fe2d5e4fa513e51a08e1d5a2
Andrew Stanton <andrew.stanton@ucla.edu>
2020-11-03 00:17:41 -0800

    Update parameters.py
    added comments

--- rotor-model/parameters.py
+++ rotor-model/parameters.py
@@ -1,3 +1,7 @@
+"""
+Default parameters used for quick start demo. 
+"""
+
 # NxN size of lattice
 N = 4
 # number of data points
@@ -12,3 +16,15 @@ equilibration_steps = 1000
 steps_to_average = 1000
 # 18.80436397 # coefficient of rotation potential
 rotational_potential = 0
+
+"""
+Suggested parameters to see equilibration based on rotational potential are: 
+  rotational_potential = 0         # expected runtime = 1.25 hours
+  N = 16
+  num_data_points = 10  
+  l_range = 0.6
+  r_range = 2.6
+  equilibration_steps = 10000
+  steps_to_average = 1000
+"""
+
