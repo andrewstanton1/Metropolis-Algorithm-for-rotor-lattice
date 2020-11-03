4add95cdb95cc00dc5d9622962f79d6f25a2511b
Jeff Stanton <jstanton397@yahoo.com>
2020-11-02 22:51:21 -0800

    more comprehensive requirements file, fixed spelling mistakes

--- rotor-model/parameters.py
+++ rotor-model/parameters.py
@@ -1,7 +1,14 @@
-N = 16                                            # NxN size of lattice
-num_data_points = 10                              # Number of data points
-l_range = 0.6                                     # starting data value
-r_range = 2.6                                     # ending data value
-equalibration_steps = 10000                       # number of monte carlo steps for equlibration 
-steps_to_average = 1000                           # number of monte carlo steps to average over
-rotational_potential = 0                          #18.80436397 # coefficient of rotation potential
+# NxN size of lattice
+N = 4
+# number of data points
+num_data_points = 10
+# starting data value
+l_range = 0.6
+# ending data value
+r_range = 2.6
+# number of monte carlo steps for equilibration
+equilibration_steps = 1000
+# number of monte carlo steps to average over
+steps_to_average = 1000
+# 18.80436397 # coefficient of rotation potential
+rotational_potential = 0
